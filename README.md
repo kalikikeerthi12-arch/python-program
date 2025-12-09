19addf70df23fe0aace02c0c082eca37565e0cf7
kalikikeerthi12-arch <kalikikeerthi12@gmail.com>
2025-12-09 20:49:10 +0530

    add_numbers.py file

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-program
-A simple and efficient python program designed to perform task quickly and accurately using clean logic and user friendly code.
---
+++ add_numbers.py
@@ -0,0 +1,9 @@
+# Program to add two numbers
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("The sum is:", sum)
+A simple and efficient python program designed to perform task quickly and accurately using clean logic and user friendly code.
